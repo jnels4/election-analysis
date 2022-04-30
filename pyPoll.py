61d88868085c30db6cc46f04f6b1a91d597afa3b
John Nelson <johne.nelsoniv@gmail.com>
2022-04-30 19:28:47 -0400

    added vote percentage and tracking

--- pyPoll.py
+++ pyPoll.py
@@ -9,11 +9,41 @@ import csv
 #save path
 fileToLoad = os.path.join('Resources','election_results.csv')
 fileToSave = os.path.join('analysis','election_analysis.txt')
+
+#initialize total votes
+totalVotes = float(0) 
+#initialize candidate options
+candidateList = []
+#dictionary to hold election data
+candidateVotes = {}
 #open election results
 with open(fileToLoad) as electionData:
+    #prepare file to be read
     fileReader = csv.reader(electionData)
+
+    #read the header row
     headers = next(fileReader)
     print(headers)
+
+    #analize our file
+    for row in fileReader:
+        totalVotes +=1
+        #add candidate names to candidate list
+        candidateName = row[2]
+        if candidateName not in candidateList:
+            candidateList.append(candidateName)
+            #track candidates 
+            candidateVotes[candidateName] = 0
+        candidateVotes[candidateName] +=1
+    print (candidateList)
+    print (candidateVotes)
+    print(totalVotes)
+    for candidateName in candidateList:
+        votePercentage = (candidateVotes[candidateName]/totalVotes)*100
+        print(f'{candidateName}: recieved {votePercentage:.2f}% of the vote.')
+
+
+
 
 #Perform Analysis
 with open(fileToSave,'w') as txt_file:
