4ef8e9e612b84477f958f718bcb863bb0029fafc
John Nelson <johne.nelsoniv@gmail.com>
2022-05-01 12:40:22 -0400

    Added county analysis

--- pyPoll.py
+++ pyPoll.py
@@ -20,6 +20,13 @@ candidateVotes = {}
 winningCandidate = ''
 winningCount = 0
 winningPercentage = 0
+
+#determine county results
+countyVotes = {}
+countyList = []
+countyVoteNum = 0
+largestCountyVotes = 0
+
 #open election results
 with open(fileToLoad) as electionData:
     #prepare file to be read
@@ -34,19 +41,41 @@ with open(fileToLoad) as electionData:
         totalVotes +=1
         #add candidate names to candidate list, avoid duplicates
         candidateName = row[2]
+        countyName = row[1]
+        if countyName not in countyList:
+            #track counties
+            countyList.append(countyName)
+            #innitialize votes to 0 for any county being added to dictioary
+            countyVotes[countyName]=0
         if candidateName not in candidateList:
             candidateList.append(candidateName)
             #track candidates 
             candidateVotes[candidateName] = 0
         #tally votes in dictionary tied to candidate name
         candidateVotes[candidateName] +=1
+        countyVotes[countyName] +=1
+    
     #write to file    
     with open (fileToSave,'w') as txt_file:   
-        totalVoteRes = (f'Election Results\n-------------------------\nTotal Votes:{int(totalVotes):,}\n-------------------------\n')
+        totalVoteRes = (f'Election Results\n-------------------------\nTotal Votes:{int(totalVotes):,}\n-------------------------\nCounty Votes:\n')
         #print(totalVoteRes, end="")
         txt_file.write(totalVoteRes)
     
-    
+    #Vote analysis for county turnour
+        for countyName in countyList:
+            countyVotePercentage = (countyVotes[countyName]/totalVotes)*100
+            countyStats = (f'{countyName}: {countyVotePercentage:.2f} ({countyVotes[countyName]})\n')
+            print (countyStats)
+            txt_file.write(countyStats)
+            if (countyVotes[countyName] > largestCountyVotes):
+                largestCountyVotes = countyVotes[countyName]
+                largestCounty = countyName
+                voterTurnout = (f'\n-------------------------\nThe largest county turnout was: {largestCounty} ({largestCountyVotes})\n-------------------------\n')
+        txt_file.write(voterTurnout)
+        print(voterTurnout)
+        
+
+
     #Vote analysis for winner & percentages
     
         for candidateName in candidateList:
